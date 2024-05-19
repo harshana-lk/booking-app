e44d6e6a886e0893463eb122df78d9e631b0d8fb
harshana-lk <harshuonlive@gmail.com>
2024-05-19 13:03:14 +0530

    🔖 Successfully created connection to the mongoDB database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,21 @@ import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// Connecting to MongoDB
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
 
+const app = express();
+app.use(express.json()); // convert the body to a json object
+app.use(express.urlencoded({ extended: true })); // convert the body to url encoded
+app.use(cors()); // allow cross origin requests
+
+// Adding an endpoint for testing
 app.get("/api/test", async (req: Request, res: Response) => {
   res.json({ message: "hello from express endpoint!" });
 });
 
+// Starting the server
 app.listen(3000, () => console.log("Server started on port 3000"));
